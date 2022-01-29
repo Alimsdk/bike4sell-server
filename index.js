f34ee54911cf7910552d90e0c2e10cff6c24b27c
Alimsdk <alimsiddique04@gmail.com>
2022-01-29 18:51:20 +0600

    env file added

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express=require('express');
 const cors=require('cors');
 const { MongoClient } = require('mongodb');
 const ObjectId=require('mongodb').ObjectId
-const uri = "mongodb+srv://bike4sell:dnu3b8dXYhuxzkwB@cluster0.ofdnr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+require('dotenv').config();
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ofdnr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const app=express();
-const port=5000
+const port= process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json());
